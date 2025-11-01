a3d770b5817528f179c81aaa8ba5524f7f1cce84
Suhail Hasan Khan <shasankhan@usf.edu>
2025-11-01 19:05:11 -0400

    Started working on the final section which is the catalog page.

---
+++ src/app/components/Catalog.jsx
@@ -0,0 +1,9 @@
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import ProductList from './ProductList';
+import CategoryFilter from './CategoryFilter';
+import PriceFilter from './PriceFilter';
+import CartSummary from './CartSummary';
+import StatusMessage from './StatusMessage';
